99bcb9955aaaaaf8b42e4e32eaaabd54e356ef7e
dralucindaodonto <dralucindaodonto@gmail.com>
2022-04-18 23:12:50 -0300

    nuxt3dentaldesk

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <n-result status="success" title="succss" description="naive-ui now perfectly supports nuxt3">
+    <n-result
+      status="success"
+      title="succss"
+      description="naive-ui now perfectly supports nuxt3"
+    >
       <template #footer>
         <n-button type="success">Button</n-button>
       </template>
@@ -18,10 +22,5 @@
       如果它长得像鸭子，走起来像鸭子，叫起来也像鸭子，那它一定是个鸭子。
     </n-tooltip>
     <n-h1>特点</n-h1>
-    <n-space>
-      <n-tag>Nuxt3</n-tag>
-      <n-tag>Vite</n-tag>
-      <n-tag>Naive-ui</n-tag>
-    </n-space>
   </div>
 </template>
